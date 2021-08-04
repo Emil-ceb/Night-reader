162e652ae9ee324f3bfc48edea7d605e29eda18e
Emil-ceb <emiliocec@outlook.com>
2021-08-03 19:13:46 -0500

    Se creo metodo para wall jump

--- Assets/Scripts/Player Control/PlayerMovement.cs
+++ Assets/Scripts/Player Control/PlayerMovement.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider=GetComponent<BoxCollider2D>();
     }
 
     private void Update() 
     {
-        float horizontalInput=Input.GetAxis("Horizontal");
-        body.velocity=new Vector2(horizontalInput*speed, body.velocity.y);
-
+        horizontalInput=Input.GetAxis("Horizontal");
+        
         //Voltea al jugador al moverse
         if(horizontalInput > 0.01f)
         {
@@ -30,26 +34,76 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale=new Vector3(-1,1,1);
         }
 
-        if(Input.GetKey(KeyCode.Space) && grounded)
-        {
-            Jump();
-        }
-
         anim.SetBool("Run",horizontalInput!=0);
-        anim.SetBool("Grounded", grounded);
+        anim.SetBool("Grounded", isGrounded());
+
+        //Codigo para generar el wall jump
+        if (wallJumpCooldown > 0.2f)
+        {
+            
+
+          body.velocity=new Vector2(horizontalInput*speed, body.velocity.y);
+
+          if (onWall() && !isGrounded())
+          {
+              body.gravityScale=0;
+              body.velocity=Vector2.zero;
+          }
+          else
+          {
+              body.gravityScale=3;
+          }
+          
+          if(Input.GetKey(KeyCode.Space))
+          {
+             Jump();
+          }
+          
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
     }
 
     private void Jump()
     {
-        body.velocity=new Vector2(body.velocity.x, jumpForce);
-        anim.SetTrigger("Jump");
-        grounded=false;
+        if(isGrounded())
+        {
+         body.velocity=new Vector2(body.velocity.x, jumpForce);
+         anim.SetTrigger("Jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity=new Vector2(-Mathf.Sign(transform.localScale.x) * 6,0);
+                transform.localScale=new Vector3 (-Mathf.Sign(transform.localScale.x), transform.localScale.y,transform.localScale.z);
+            }
+            else
+            {
+            //Esto hace que el jugador sea empujado al lado opuesto al que este viendo
+            //Mathf me ayuda a mandar los valores, en este caso volviendolos opuestos
+                body.velocity=new Vector2(-Mathf.Sign(transform.localScale.x) * 3,6);
+            }
+            wallJumpCooldown=0;
+            
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision) 
     {
-        if(collision.gameObject.tag=="Ground"){
-            grounded=true;
-        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit=Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit=Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
