cbe85085d2d7e423add96fac29de693e75220f29
Emil-ceb <emiliocec@outlook.com>
2021-08-03 22:06:49 -0500

    Se creo script para el proyectil

--- Assets/Scripts/Player Control/Proyectile.cs
+++ Assets/Scripts/Player Control/Proyectile.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Proyectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private float direction;
+    private bool hit;
+    private Animator anim;
+    private BoxCollider2D bCollide;
+    
+    private void Awake() 
     {
-        
+        anim = GetComponent<Animator>();
+        bCollide=GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        if(hit)return;
+        float movementSpeed = speed*Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        hit = true;
+        bCollide.enabled = false;
+        anim.SetTrigger("Explode");
+    }
+
+    public void SetDirection(float _direction)
     {
-        
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        bCollide.enabled = true;
+
+        float localScaleX=transform.localScale.x;
+
+        if(Mathf.Sign(localScaleX) !=_direction)
+        {
+            localScaleX= -localScaleX;
+        }
+        transform.localScale = new Vector3 (localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
