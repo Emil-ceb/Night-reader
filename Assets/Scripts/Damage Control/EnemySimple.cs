a2ca996fd999464f02a15686bb807f3731b27d97
Emil-ceb <emiliocec@outlook.com>
2021-08-05 00:42:49 -0500

    modificacion a escena

--- Assets/Scripts/Damage Control/EnemySimple.cs
+++ Assets/Scripts/Damage Control/EnemySimple.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class EnemySimple : MonoBehaviour
 {
+    [SerializeField] private float movementDistance;
+    [SerializeField] private float speed;
     [SerializeField] private float damage;
+    private bool movingLeft;
+    private float leftSide;
+    private float rightSide;
+
+    private void Awake() 
+    {
+        leftSide = transform.position.x - movementDistance;
+        rightSide = transform.position.x + movementDistance;
+    }
+
+    private void Update() 
+    {
+        if (movingLeft)
+        {
+            if(transform.position.x > leftSide)
+            {
+                 transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = false;
+            }
+        }
+        else
+        {
+            if(transform.position.x < rightSide)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                movingLeft = true;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) 
     {
